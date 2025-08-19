e708d7f6d43dbac03495ef6e0aefc6be261ff76b
Hoàng Quốc Việt <vieth1393@gmail.com>
2025-08-19 21:29:42 +0700

    Update MacroCore.lua

--- modules/MacroCore.lua
+++ modules/MacroCore.lua
@@ -1,13 +1,1 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/sojun/main/modules/Orion.lua"))()
 
-local Window = OrionLib:MakeWindow({
-    Name = "Sojun Hub - Garden Tower Defense",
-    HidePremium = false,
-    SaveConfig = true,
-    ConfigFolder = "SojunHub"
-})
-
--- Load Macro tab
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sojun/main/tabs/MacroTab.lua"))()(Window)
-
-OrionLib:Init()
